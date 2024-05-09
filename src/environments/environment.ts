6699823770366103695d29aec03bc296a65a1195
Janira Santamaria <janira.santamaria@econoparts.com.sv>
2024-05-09 10:15:38 -0600

    update repository

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -12,11 +12,11 @@ export const environment2 = {
 };
 
 export const environment = {
-  production: true,
-  ip: "sorteovalvoline.econoparts.com:4433",
-  //ip: "localhost:49220",
-  //protocolo: "http",
-  protocolo: "https",
+  production: false,
+  //ip: "sorteovalvoline.econoparts.com:4433",
+  ip: "localhost:49220",
+  protocolo: "http",
+  //protocolo: "https",
   version: "v1.6.3",
   signature: "46b3dd93-27a3-42cd-8cdd-6b431a7667f5"
 };
